8b1a1614f02d84ca41874fe6449b8890bb3e5b81
Abigail Konadu <abigailakonadu@gmail.com>
2023-02-22 10:52:37 -0800

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_10_alphabets - printlower alpha ten times
+ * Return: 0
+ */
+void print_10_alphabets(void)
+{
+	for (int i = 1; i <= 10; i++)
+	{
+	for (char a = 'a'; a <= 'z'; a++)
+	}
+putchar(a);
+}
+}
+putchar('\n');
+}
