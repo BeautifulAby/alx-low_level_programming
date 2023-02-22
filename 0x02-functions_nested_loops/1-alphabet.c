ff92afe41fa7b7d869290438e23e0a65cc19999c
Abigail Konadu <abigailakonadu@gmail.com>
2023-02-22 05:08:18 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
