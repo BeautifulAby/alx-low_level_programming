db28f7f992dc7ddd06e5d922a668cc296dde7748
Abigail Konadu <abigailakonadu@gmail.com>
2023-02-28 16:57:17 -0800

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,17 +13,17 @@ void puts2(char *str)
 	int o;
 
 	while (*y != '\0')
-	{
+
 		y++;
 		longi++;
-	}
+
 	t = longi - 1;
+
 	for (o = 0; o <= t; o++)
-	{
+
 		if (o % 2 == 0)
-	{
+
 		_putchar(str[o]);
-	}
-		_putchar('\n');
-	}
+	_putchar('\n');
+
 }
